a031791504a2d1fd4ffc9342c167add3949fea6d
Kaidan <fishstacks@protonmail.com>
2018-03-18 23:14:01 +1100

    Cleaned up error handling code and added verbose argument

--- src/main.rs
+++ src/main.rs
@@ -11,46 +11,41 @@ use structopt::StructOpt;
 #[derive(StructOpt, Debug)]
 #[structopt(name = "rnmusic", about = "A small utility to strip youtube URLs from filenames")]
 struct Opt {
-    /// Path to search for files
+    /// Path containing files to be renamed
     #[structopt(short = "p", long = "path", parse(from_os_str))]
     path: PathBuf,
+    /// Verbose output
+    #[structopt(short = "v", long = "verbose")]
+    verbose: bool,
 }
 
-fn rename_files(src_path: PathBuf) {
+fn rename_files(src_path: &PathBuf, verbose: bool) {
     let re = Regex::new(r"(?i)-([a-z0-9-_]+)\.mp3").expect("Failed to compile regex");
-    let file_entries = match fs::read_dir(&src_path) {
-        Ok(file_entries) => file_entries,
-        Err(error) => panic!("Error: Failed to open directory ({:?})", error.kind()),
-    };
-
+    let file_entries = fs::read_dir(&src_path).expect("Failed to read directory");
+    
     for file_entry in file_entries {
-        let file = match file_entry {
-            Ok(file) => file,
-            Err(error) => panic!(
-                "Error: Failed to read directory contents ({:?})",
-                error.kind()
-            ),
-        };
-        let filename = match file.file_name().into_string() {
-            Ok(filename) => filename,
-            Err(os_string) => panic!(
-                "Error: Failed to convert filename ({:?}) to string",
-                os_string
-            ),
-        };
-        let newfile = Path::new(&src_path)
-            .join(
-                re.replace(&filename, ".mp3").to_string()
-            );
+        let filename = file_entry
+            .expect("Failed to read file")
+            .file_name()
+            .into_string()
+            .expect("Failed to convert filename to string");
+        let newfile = Path::new(&src_path).join(re.replace(&filename, ".mp3").as_ref());
         let originalfile = Path::new(&src_path).join(&filename);
-        match fs::copy(&originalfile, &newfile) {
-            Ok(_) => println!("Renamed {}", filename),
-            Err(error) => panic!("Failed to copy {} to {:?} with error {:?}", filename, newfile, error),
+        if verbose {
+            match fs::copy(&originalfile, &newfile) {
+                Ok(_) => println!("Renamed {}", filename),
+                Err(error) => panic!(
+                    "Failed to copy {} to {:?} with error {:?}",
+                    filename, newfile, error
+                ),
+            }
+        } else {
+            fs::copy(&originalfile, &newfile).expect("Failed to copy file");
         }
     }
 }
 
 fn main() {
     let args = Opt::from_args();
-    rename_files(args.path);
+    rename_files(&args.path, args.verbose);
 }
