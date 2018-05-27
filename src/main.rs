326eef7fb66d82bfc894db7f9722cb81336c259c
Kaidan <fishstacks@protonmail.com>
2018-05-27 21:17:24 +1000

    Refactored code, added logging module
    Code was refactored to remove the rename_files() function
    Started using simplelog framework, simplified logging
    Added --delete, although right now it does nothing
    Added --quiet, suppress output of logging framework
    Updated dependancy versions

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 #[macro_use]
 extern crate structopt;
 extern crate regex;
+#[macro_use]
+extern crate log;
+extern crate simplelog;
 
-use std::fs;
-use std::path::PathBuf;
-use std::path::Path;
 use regex::Regex;
+use simplelog::{Config, LevelFilter, TermLogger};
+use std::fs;
+use std::path::Path;
+use std::path::PathBuf;
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
@@ -17,14 +21,26 @@ struct Opt {
     /// Optional output directory, if specified renamed files will be placed here
     #[structopt(short = "o", long = "output", parse(from_os_str))]
     output: Option<PathBuf>,
-    /// Verbose output
-    #[structopt(short = "v", long = "verbose")]
-    verbose: bool,
+    /// Show less output
+    #[structopt(short = "q", long = "quiet")]
+    quiet: bool,
+    /// Delete original files after renaming them
+    #[structopt(short = "d", long = "delete")]
+    delete: bool,
 }
 
-fn rename_files(src_dir: &PathBuf, out_dir: &PathBuf, verbose: bool) {
+fn main() {
+    let args = Opt::from_args();
+    if args.quiet {
+        TermLogger::init(LevelFilter::Warn, Config::default())
+            .expect("Failed to initialize logger");
+    } else {
+        TermLogger::init(LevelFilter::Info, Config::default())
+            .expect("Failed to initialize logger");
+    }
+    let output_dir = args.output.unwrap_or(args.source.clone());
     let re = Regex::new(r"(?i)-([a-z0-9-_]+)\.mp3").expect("Failed to compile regex");
-    let file_entries = fs::read_dir(&src_dir).expect("Failed to read directory");
+    let file_entries = fs::read_dir(&args.source).expect("Failed to read directory");
 
     for file_entry in file_entries {
         let filename = file_entry
@@ -37,24 +53,15 @@ fn rename_files(src_dir: &PathBuf, out_dir: &PathBuf, verbose: bool) {
         if !re.is_match(&filename) {
             continue;
         }
-        let newfile = Path::new(&out_dir).join(re.replace(&filename, ".mp3").as_ref());
-        let originalfile = Path::new(&src_dir).join(&filename);
-        if verbose {
-            match fs::copy(&originalfile, &newfile) {
-                Ok(_) => println!("Renamed {}", filename),
-                Err(error) => panic!(
-                    "Failed to copy {:?} to {:?} with error {:?}",
-                    originalfile, newfile, error
-                ),
-            }
-        } else {
-            fs::copy(&originalfile, &newfile).expect("Failed to copy file");
+
+        let newfile = Path::new(&output_dir).join(re.replace(&filename, ".mp3").as_ref());
+        let originalfile = Path::new(&args.source).join(&filename);
+        match fs::copy(&originalfile, &newfile) {
+            Ok(_) => info!("Renamed {}", filename),
+            Err(error) => panic!(
+                "Failed to copy {:?} to {:?} with error {:?}",
+                originalfile, newfile, error
+            ),
         }
     }
-}
-
-fn main() {
-    let args = Opt::from_args();
-    let output_dir = args.output.unwrap_or(args.source.clone());
-    rename_files(&args.source, &output_dir, args.verbose);
 }
